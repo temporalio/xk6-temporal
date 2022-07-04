5211d2a289816c9ff249e0378592e9312624d87a
Rob Holland <rob@temporal.io>
2022-07-04 17:47:59 +0100

    Add a more aggressive example.

---
+++ examples/single-activity-10k.js
@@ -0,0 +1,31 @@
+import temporal from 'k6/x/temporal';
+import { scenario } from 'k6/execution';
+
+export const options = {
+    scenarios: {
+      contacts: {
+        executor: 'shared-iterations',
+        iterations: '10000',
+        vus: 100,
+      },
+    },
+};
+
+export default () => {
+    const client = temporal.newClient({ HostPort: __ENV.TEMPORAL_GRPC_ENDPOINT })
+
+    const handle = client.startWorkflow(
+        {
+            namespace: 'default',
+            task_queue: 'benchmark',
+            id: 'wf-' + scenario.iterationInTest,
+        },
+        'SingleActivityWorkflow',
+        'bob',
+    )
+
+    // Wait until the workflow has completed.
+    const result = handle.result()
+
+    client.close()
+};
